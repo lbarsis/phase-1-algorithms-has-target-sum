6f4d6872ad441d641ef4be6e1bdb404f6270630c
Levi Barsis <lbarsis@gmail.com>
2022-08-30 19:36:27 -0700

    added algorithm based on solution from canvas

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
+// function hasTargetSum(array, target) {
+//   // Write your algorithm here
+//   let arrSum = array.flatMap((a, i) => array.slice(i+1).map(b => a + b))
+//   // let arrSum = array.flatMap((i) => array.slice(i+1))
+//   return arrSum.includes(target)
+//   // return arrSum
+// }
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-  let arrSum = array.flatMap((a, i) => array.slice(i+1).map(b => a + b))
-  // let arrSum = array.flatMap((i) => array.slice(i+1))
-  return arrSum.includes(target)
-  // return arrSum
+  const seenNumbers ={}
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in seenNumbers) {
+      return true;
+    } else {
+      seenNumbers[number] = true
+    }
+  }
+  return false
 }
+
+// hasTargetSum([3, 8, 12, 4, 11, 7], 10)
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  make function 'hasTargetSum'
+
   get all possibilities of the array and assign it to variable
   search each number for the target
   (I know! It's not efficient at all, I can't think of another solution!)
